0eec3b5fed6c8dd9db9fe8f545b13ba47b34ae22
Rowan <104235853+roesorcerer@users.noreply.github.com>
2024-07-07 19:11:15 -0500

    Update next.config.mjs

--- qr-code/next.config.mjs
+++ qr-code/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'dist/qr-code',
+  basePath: '/frontend-practice', // Ensure this is set correctly if you are using a base path
+};
 
-export default nextConfig;
+module.exports = nextConfig;
