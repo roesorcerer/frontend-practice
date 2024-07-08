579a0510027090108f63018595c1f87880b9b7a0
Rowan <104235853+annastratton@users.noreply.github.com>
2024-07-08 13:36:49 -0500

    updating next setting for export

--- qr-code/next.config.mjs
+++ qr-code/next.config.mjs
@@ -1,8 +1,12 @@
 // qr-code/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist/qr-code',
-  basePath: '/frontend-practice/qr-code', // Ensure this is set correctly if you are using a base path
+  distDir: "dist/qr-code",
+  basePath: "/frontend-practice/qr-code", // Ensure this is set correctly if you are using a base path
+  output: "export",
+  images: {
+    unoptimized: true, // Add this if you are not using optimized images
+  },
 };
 
 export default nextConfig;
